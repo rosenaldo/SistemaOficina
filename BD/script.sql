f99b05ac41b2248aab8c6a54ae38612b2802c79a
Jose Rosenaldo da S Junior <joserdasjunior@gmail.com>
2025-04-23 17:32:42 -0300

    Banco de dados

---
+++ BD/script.sql
@@ -0,0 +1,343 @@
+-- oficina.vendas definition
+
+CREATE TABLE `vendas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `produto` int(11) NOT NULL,
+  `valor` decimal(8,2) NOT NULL,
+  `funcionario` varchar(20) NOT NULL,
+  `data` date NOT NULL,
+  `id_orc` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=62 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+
+-- oficina.veiculos definition
+
+CREATE TABLE `veiculos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `marca` varchar(30) NOT NULL,
+  `modelo` varchar(30) NOT NULL,
+  `cor` varchar(30) NOT NULL,
+  `placa` varchar(20) NOT NULL,
+  `ano` int(11) NOT NULL,
+  `km` int(11) NOT NULL,
+  `cliente` varchar(20) NOT NULL,
+  `data` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+
+-- oficina.usuarios definition
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) NOT NULL,
+  `cpf` varchar(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `senha` varchar(30) NOT NULL,
+  `nivel` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- oficina.tipo_pcm definition
+
+CREATE TABLE `tipo_pcm` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `descricao` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=60 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+
+-- oficina.servicos definition
+
+CREATE TABLE `servicos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) NOT NULL,
+  `valor` decimal(8,2) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+
+-- oficina.retornos definition
+
+CREATE TABLE `retornos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `veiculo` int(11) NOT NULL,
+  `data_serv` date NOT NULL,
+  `data_contato` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+
+-- oficina.recepcionistas definition
+
+CREATE TABLE `recepcionistas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) NOT NULL,
+  `cpf` varchar(20) NOT NULL,
+  `telefone` varchar(20) DEFAULT NULL,
+  `email` varchar(50) NOT NULL,
+  `endereco` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+
+-- oficina.produtos definition
+
+CREATE TABLE `produtos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(80) NOT NULL,
+  `categoria` int(11) NOT NULL,
+  `fornecedor` int(11) NOT NULL,
+  `valor_compra` decimal(8,2) NOT NULL,
+  `valor_venda` decimal(8,2) NOT NULL,
+  `estoque` int(11) NOT NULL,
+  `descricao` text DEFAULT NULL,
+  `imagem` varchar(100) DEFAULT NULL,
+  `nivel_min` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+
+-- oficina.pcm_preventiva definition
+
+CREATE TABLE `pcm_preventiva` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pcm` int(11) NOT NULL,
+  `servico` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=118 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+
+-- oficina.pcm_preditiva definition
+
+CREATE TABLE `pcm_preditiva` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pcm` int(11) NOT NULL,
+  `servico` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=89 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+
+
+
+-- oficina.pcm_corretiva definition
+
+CREATE TABLE `pcm_corretiva` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pcm` int(11) NOT NULL,
+  `servico` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=103 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+
+-- oficina.pcm definition
+
+CREATE TABLE `pcm` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cliente` varchar(20) NOT NULL,
+  `veiculo` int(11) NOT NULL,
+  `descricao` text NOT NULL,
+  `servico` int(11) NOT NULL,
+  `data` date NOT NULL,
+  `mecanico` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=50 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+
+-- oficina.os definition
+
+CREATE TABLE `os` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `descricao` varchar(50) NOT NULL,
+  `valor` decimal(8,2) NOT NULL,
+  `mecanico` varchar(20) NOT NULL,
+  `cliente` varchar(20) NOT NULL,
+  `data_entrega` date NOT NULL,
+  `concluido` varchar(5) NOT NULL,
+  `valor_mao_obra` decimal(8,2) NOT NULL,
+  `data` date NOT NULL,
+  `veiculo` int(11) NOT NULL,
+  `garantia` int(11) DEFAULT NULL,
+  `obs` text NOT NULL,
+  `tipo` varchar(20) NOT NULL,
+  `id_orc` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=96 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+
+-- oficina.orcamentos definition
+
+CREATE TABLE `orcamentos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cliente` varchar(20) NOT NULL,
+  `veiculo` int(11) NOT NULL,
+  `descricao` text NOT NULL,
+  `valor` decimal(8,2) NOT NULL,
+  `servico` int(11) NOT NULL,
+  `data` date NOT NULL,
+  `data_entrega` date NOT NULL,
+  `garantia` int(11) NOT NULL,
+  `mecanico` varchar(20) NOT NULL,
+  `obs` text NOT NULL,
+  `status` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=59 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+
+-- oficina.orc_serv definition
+
+CREATE TABLE `orc_serv` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `orcamento` int(11) NOT NULL,
+  `servico` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=90 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+
+-- oficina.orc_prod definition
+
+CREATE TABLE `orc_prod` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `orcamento` int(11) NOT NULL,
+  `produto` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=98 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+
+-- oficina.movimentacoes definition
+
+CREATE TABLE `movimentacoes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tipo` varchar(20) NOT NULL,
+  `descricao` varchar(50) NOT NULL,
+  `valor` decimal(8,2) NOT NULL,
+  `funcionario` varchar(20) NOT NULL,
+  `data` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+
+-- oficina.mecanicos definition
+
+CREATE TABLE `mecanicos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) NOT NULL,
+  `cpf` varchar(20) NOT NULL,
+  `telefone` varchar(20) DEFAULT NULL,
+  `email` varchar(50) NOT NULL,
+  `endereco` varchar(150) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+
+-- oficina.fornecedores definition
+
+CREATE TABLE `fornecedores` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) NOT NULL,
+  `tipo_pessoa` varchar(20) NOT NULL,
+  `cpf` varchar(20) NOT NULL,
+  `telefone` varchar(20) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `endereco` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+
+-- oficina.controles definition
+
+CREATE TABLE `controles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `veiculo` int(11) NOT NULL,
+  `mecanico` varchar(20) NOT NULL,
+  `data` date NOT NULL,
+  `descricao` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+
+-- oficina.contas_receber definition
+
+CREATE TABLE `contas_receber` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `descricao` varchar(50) NOT NULL,
+  `valor` decimal(8,2) NOT NULL,
+  `adiantamento` decimal(8,2) DEFAULT NULL,
+  `mecanico` varchar(20) NOT NULL,
+  `cliente` varchar(20) NOT NULL,
+  `funcionario` varchar(20) DEFAULT NULL,
+  `data` date NOT NULL,
+  `pago` varchar(5) NOT NULL,
+  `id_servico` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=84 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+
+-- oficina.contas_pagar definition
+
+CREATE TABLE `contas_pagar` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `descricao` varchar(50) NOT NULL,
+  `valor` decimal(8,2) NOT NULL,
+  `funcionario` varchar(20) NOT NULL,
+  `data` date NOT NULL,
+  `data_venc` date NOT NULL,
+  `pago` varchar(5) NOT NULL,
+  `imagem` varchar(100) DEFAULT NULL,
+  `fornecedor` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=49 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+
+-- oficina.compras definition
+
+CREATE TABLE `compras` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `produto` int(11) NOT NULL,
+  `valor` decimal(8,2) NOT NULL,
+  `funcionario` varchar(20) NOT NULL,
+  `data` date NOT NULL,
+  `id_conta` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+
+-- oficina.comissoes definition
+
+CREATE TABLE `comissoes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `valor` decimal(8,2) NOT NULL,
+  `mecanico` varchar(20) NOT NULL,
+  `tipo` varchar(20) NOT NULL,
+  `id_servico` int(11) NOT NULL,
+  `data` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+
+-- oficina.clientes definition
+
+CREATE TABLE `clientes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) NOT NULL,
+  `cpf` varchar(20) NOT NULL,
+  `telefone` varchar(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `endereco` varchar(100) NOT NULL,
+  `data` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+
+-- oficina.categorias definition
+
+CREATE TABLE `categorias` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+
+
+
